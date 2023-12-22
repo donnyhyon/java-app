bcb6f9658f6661a6739283daaaa4173a04b7a3cf
donnyhyon <dhyon333@hotmail.co.uk>
2023-12-22 23:12:20 +0000

    created employee class

---
+++ src/main/java/com/example/demo/employee/Employee.java
@@ -0,0 +1,12 @@
+package com.example.demo.employee;
+
+public class Employee {
+    private String name;
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
